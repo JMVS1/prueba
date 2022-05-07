ac421a97f82cdebb4ee523732ffff727914ca782
JMVS1 <105028184+JMVS1@users.noreply.github.com>
2022-05-06 21:50:56 -0500

    segundo cambio

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>Hello Juan !</h1>
+<h1>Hello Juan Martin!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
